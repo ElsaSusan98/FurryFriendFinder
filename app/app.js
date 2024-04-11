57bdba832cd62f049dbcd46961719057f4345f05
David <davidpious7@gmail.com>
2024-04-11 14:05:43 +0100

    contact us page created

--- app/app.js
+++ app/app.js
@@ -14,14 +14,19 @@ app.set("views", "./app/views");
 // Example middleware to handle CORS headers
 app.use((req, res, next) => {
   // Allow requests from all origins during development
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   // Set other CORS headers as needed
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
   // Continue to the next middleware
   next();
 });
-
 
 app.get("/", homeController.index);
 app.get("/login", homeController.index);
@@ -32,6 +37,7 @@ app.post("/register", authController.register);
 app.get("/blog", homeController.blog);
 app.get("/aboutus", homeController.aboutUs);
 app.get("/finder", homeController.finder);
+
 app.get("/goodbye", (req, res) => res.send("Goodbye world!"));
 app.get("/hello/:name", (req, res) => res.send("Hello " + req.params.name));
 
